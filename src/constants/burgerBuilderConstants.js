521397ee55e56bad36ac6fac9296151f774e18bc
jezowski <kajetanjezowski@gmail.com>
2021-02-09 21:13:40 +0100

    Add missing constants dir

---
+++ src/constants/burgerBuilderConstants.js
@@ -0,0 +1,17 @@
+export const INGREDIENT_PRICES = {
+    onion: 0.1,
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7,
+    tomato: 0.3
+}
+
+export const MAX_INGREDIENT_VAL = {
+    onion: 4,
+    salad: 4,
+    cheese: 4,
+    meat: 6,
+    bacon: 4,
+    tomato: 4
+}
